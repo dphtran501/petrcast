4e7c583debe764d6569ad1ffd5763b65e1025a66
Derek Tran <dphtran501@gmail.com>
2019-11-14 22:25:19 -0800

    Add alert pop up, alert textbox, and Darksky attribution

--- public/scripts.js
+++ public/scripts.js
@@ -1,9 +1,29 @@
 const app = document.getElementById('root')
+
+const alertDialog = document.createElement('div')
+alertDialog.setAttribute('class', 'alert-dialog')
+alertDialog.style.display = 'none'
+
 const container = document.createElement('div')
 container.setAttribute('class', 'container')
-app.appendChild(container)
 
-fetch('weather', {
+const darkskyAttribution = document.createElement('a')
+darkskyAttribution.setAttribute('class', 'darksky-logo')
+darkskyAttribution.href = "https://darksky.net/poweredby/"
+darkskyAttribution.text = "Powered by Dark Sky"
+
+const alertModal = document.createElement('div')
+alertModal.setAttribute('class', 'modal')
+alertModal.style.display = 'none'
+
+app.appendChild(alertDialog)
+app.appendChild(container)
+app.appendChild(darkskyAttribution)
+
+app.appendChild(alertModal)
+
+//fetch('testData.json', {        // Use for testing
+fetch('weather', {            // Use for production
     method: 'GET',
     headers: {
         'Content-Type': 'application/json',
@@ -40,9 +60,48 @@ fetch('weather', {
     })
 
     function setAlerts(alerts){
-        if(alerts != null)
-        {
+        if(alerts != null){
+
+            const alertModalContent = document.createElement('div')
+            alertModalContent.setAttribute('class', 'modal-content')
+            const alertTitle = document.createElement('h1')
+            alertTitle.textContent = alerts[0].title
+            const alertDescription = document.createElement('p')
+            alertDescription.textContent = alerts[0].description
+            const alertLink = document.createElement('a')
+            alertLink.href = alerts[0].uri
+            alertLink.text = "See more details..."
             
+            alertModalContent.appendChild(alertTitle)
+            alertModalContent.appendChild(alertDescription)
+            alertModalContent.appendChild(alertLink)
+
+            alertModal.appendChild(alertModalContent)
+            alertModal.style.display = 'block'
+
+            alerts.forEach(alert => {
+                const alertText = document.createElement('p')
+                alertText.textContent = alert.severity + ": " + alert.title
+                alertDialog.appendChild(alertText)
+            })
+            
+            alertDialog.style.display = "block"
         }
-            
+        else {
+            while(alertDialog.lastChild){
+                alertDialog.removeChild(alertDialog.lastChild)
+            }
+            while(alertModal.lastChild){
+                alertModal.removeChild(alertModal.lastChild)
+            }
+
+            alertModal.style.display = 'none'
+            alertDialog.style.display = "none"
+        }
+    }
+
+    window.onclick = function(event){
+        if (event.target == alertModal) {
+            alertModal.style.display = "none"
+        }
     }
